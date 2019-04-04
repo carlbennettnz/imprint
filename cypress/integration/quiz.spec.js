4159f0a530f1ce9134de1481840013e5a54e99c5
Carl Bennett <carlbennettnz@gmail.com>
2019-04-04 13:41:31 +1300

    Show next item after guess

--- cypress/integration/quiz.spec.js
+++ cypress/integration/quiz.spec.js
@@ -14,4 +14,12 @@ context('Quiz', () => {
     cy.get('[data-test-quiz-form]').submit()
     cy.get('[data-test-quiz-input]').should('not.exist')
   })
+
+  it('shows a new item after pressing return on feedback screen', () => {
+    cy.get('[data-test-quiz-input]').type('you')
+    cy.get('[data-test-quiz-form]').submit()
+    cy.get('body').type('{enter}')
+    cy.get('[data-test-pinyin]').should('have.text', 'nǐ')
+    cy.get('[data-test-characters]').should('have.text', '你')
+  })
 })
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -10,15 +10,42 @@ enum GuessStatus {
   wrong
 }
 
-const LESSON: QuizItem[] = [{ zh: { pinyin: 'wǒ', characters: '我' }, en: ['I', 'me', 'myself'] }]
+const LESSON: QuizItem[] = [
+  { zh: { pinyin: 'wǒ', characters: '我' }, en: ['I', 'me', 'myself'] },
+  { zh: { pinyin: 'nǐ', characters: '你' }, en: ['you'] }
+]
+
+const ENTER_KEYCODE = 13
 
 type AppState = {
   guessStatus: GuessStatus | null
+  currentIndex: number
 }
 
 export class App extends Component<null, AppState> {
   state = {
-    guessStatus: null
+    guessStatus: null,
+    currentIndex: 0
+  }
+
+  componentWillMount() {
+    document.addEventListener('keypress', this.handleKeypress.bind(this))
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeypress.bind(this))
+  }
+
+  handleKeypress(event: KeyboardEvent) {
+    if (event.keyCode !== ENTER_KEYCODE) return
+    if (this.state.guessStatus === null) return
+
+    event.preventDefault()
+
+    this.setState({
+      guessStatus: null,
+      currentIndex: (this.state.currentIndex + 1) % LESSON.length
+    })
   }
 
   checkAnswer(item: QuizItem, answer: string) {
@@ -29,14 +56,15 @@ export class App extends Component<null, AppState> {
 
   render() {
     const isSubmitted = this.state.guessStatus
+    const item = LESSON[this.state.currentIndex]
 
     return (
       <div>
         <h1>Imprint</h1>
 
-        {isSubmitted && <Feedback item={LESSON[0]} />}
-        <Card {...LESSON[0].zh} />
+        <Card {...item.zh} />
         {!isSubmitted && <QuizInput submit={answer => this.checkAnswer(LESSON[0], answer)} />}
+        {isSubmitted && <Feedback item={item} />}
       </div>
     )
   }
