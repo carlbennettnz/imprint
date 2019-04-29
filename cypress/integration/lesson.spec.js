55d51defe74000bfc40412002b80be0410634b4a
Carl Bennett <carlbennettnz@gmail.com>
2019-04-30 08:43:37 +1200

    Text lesson contents is shown

---
+++ cypress/integration/lesson.spec.js
@@ -0,0 +1,35 @@
+/// <reference types="Cypress" />
+
+context.only('Lesson', () => {
+  it('has the correct title', () => {
+    cy.visit('/lessons/1')
+    cy.get('h2').should('have.text', 'Lesson 1: How are you?')
+  })
+
+  it('lists the lessons contents', () => {
+    cy.visit('/lessons/1')
+    matchLessonItem(0, { characters: '我', pinyin: 'wǒ', meaning: 'I, me, myself' })
+    matchLessonItem(1, { characters: '你', pinyin: 'nǐ', meaning: 'you' })
+    cy.get('tr').should('have.length', 2)
+  })
+})
+
+const matchLessonItem = (index, expected) => {
+  const tds = () =>
+    cy
+      .get('tr')
+      .eq(index)
+      .children('td')
+
+  tds()
+    .eq(0)
+    .should('have.text', expected.characters)
+
+  tds()
+    .eq(1)
+    .should('have.text', expected.pinyin)
+
+  tds()
+    .eq(2)
+    .should('have.text', expected.meaning)
+}
