59ee9a0e2d5b4f637c3e58c895bb7a873fd8d016
Carl Bennett <carlbennettnz@gmail.com>
2019-12-07 13:21:06 +1300

    Improve summary card behaviour when there aren't many or any words

--- src/pages/dashboard/summary-card.component.tsx
+++ src/pages/dashboard/summary-card.component.tsx
@@ -10,24 +10,38 @@ export const SummaryCard = ({ summary }: SummaryCardProps) => {
 
   return (
     <div className="bg-white border border-grey-light rounded overflow-hidden flex mb-8">
-      <div
-        className="border-green border-b-8 p-4 whitespace-no-wrap overflow-hidden truncate"
-        style={{ width: `${(summary.learned / total) * 100}%` }}
-      >
-        {summary.learned} words memorised
-      </div>
-      <div
-        className="border-orange border-b-8 p-4 whitespace-no-wrap overflow-hidden truncate ml-1"
-        style={{ width: `${(summary.needsReview / total) * 100}%` }}
-      >
-        {summary.needsReview} need revision
-      </div>
-      <div
-        className="border-gray border-b-8 p-4 whitespace-no-wrap overflow-hidden truncate ml-1"
-        style={{ width: `${(summary.unlearned / total) * 100}%` }}
-      >
-        {summary.unlearned} to learn
-      </div>
+      {summary.learned ? (
+        <div
+          className="border-green border-b-8 p-4 whitespace-no-wrap overflow-hidden truncate"
+          style={{ width: `${(summary.learned / total) * 100}%` }}
+        >
+          {summary.learned} words memorised
+        </div>
+      ) : null}
+
+      {summary.needsReview ? (
+        <div
+          className="border-orange border-b-8 p-4 whitespace-no-wrap overflow-hidden truncate"
+          style={{ width: `${(summary.needsReview / total) * 100}%` }}
+        >
+          {summary.needsReview} need revision
+        </div>
+      ) : null}
+
+      {summary.unlearned ? (
+        <div
+          className="border-gray border-b-8 p-4 whitespace-no-wrap overflow-hidden truncate"
+          style={{ width: `${(summary.unlearned / total) * 100}%` }}
+        >
+          {summary.unlearned} to learn
+        </div>
+      ) : null}
+
+      {!summary.learned && !summary.needsReview && !summary.unlearned ? (
+        <div className="p-4 text-grey-dark w-full text-sm text-center">
+          Get started by adding some words
+        </div>
+      ) : null}
     </div>
   )
 }
