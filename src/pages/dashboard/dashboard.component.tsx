04f5adf7a02f03c64688f8db9e56832c25f0549f
Carl Bennett <carlbennettnz@gmail.com>
2019-12-07 13:20:17 +1300

    Fix horizontal collapsing of dashboard when empty

--- src/pages/dashboard/dashboard.component.tsx
+++ src/pages/dashboard/dashboard.component.tsx
@@ -64,7 +64,7 @@ export const Dashboard = () => {
   }, [])
 
   return (
-    <div className="max-w-lg mx-auto mt-4 px-8">
+    <div className="max-w-lg w-full mx-auto mt-4 px-8">
       <header className="flex items-center mb-8">
         <h1 className="max-w-lg mr-auto font-sans text-2xl my-0 text-blue">Imprint</h1>
         <div>
